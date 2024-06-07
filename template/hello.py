1f5e8cca35c7e80f2a14f2b5128bf611395605d9
Zach <80639692+Zach-GH@users.noreply.github.com>
2024-06-06 23:07:06 -0400

    initial project setup test 1 (#1)
    * adding gitignore updating the readme and creating a template project
    
    * adding space and changing picture

---
+++ template/hello.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+Your Name - Zachary Meisner
+
+name of file - hello.py
+
+Brief description of file -
+
+This file is used to give a template example for code contributions.
+To run this file, navigate to the template folder in your CLI interface
+and run the command `make project`.  This will run the corresponding
+commands detailed in the Makefile and run hello.py from your terminal.
+"""
+
+# A brief description of the function written below.  This function says Hello World!
+def say_hi():
+    print("\n Hello World! \n")
+
+say_hi()
