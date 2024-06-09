2712539801c091157757f31176b54fa18302186a
Zach <zmeisne1@jh.edu>
2024-06-09 10:55:46 -0400

    adding imports and a small explanation of shebang

--- template/hello.py
+++ template/hello.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 """
+This comment block normally would be at the top of the file
+but for the sake of having a shebang we make an acception
+because it is only neccesary to have one in the main file.
+
 Your Name - Zachary Meisner
 
 name of file - hello.py
@@ -12,6 +16,9 @@ To run this file, navigate to the template folder in your CLI interface
 and run the command `make project`.  This will run the corresponding
 commands detailed in the Makefile and run hello.py from your terminal.
 """
+
+# import 1
+# import 2
 
 # A brief description of the function written below.  This function says Hello World!
 def say_hi():
